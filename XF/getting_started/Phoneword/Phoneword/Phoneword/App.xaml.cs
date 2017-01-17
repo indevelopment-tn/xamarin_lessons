d2d516fcb58ea20c7d3a38452a61c8b3e258f39b
David Baldwin <rti4.dev@gmail.com>
2017-01-17 01:32:47 -0600

    The common code for the application is now complete. Platform-specific phone dialer code will now be implemented as a DependencyService.

--- XF/getting_started/Phoneword/Phoneword/Phoneword/App.xaml.cs
+++ XF/getting_started/Phoneword/Phoneword/Phoneword/App.xaml.cs
@@ -1,4 +1,34 @@
-﻿using System;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Phoneword
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
+/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -32,3 +62,4 @@ namespace Phoneword
         }
     }
 }
+*/
---
+++ XF/getting_started/Phoneword/Phoneword/Phoneword/IDialer.cs
@@ -0,0 +1,20 @@
+﻿namespace Phoneword
+{
+    public interface IDialer
+    {
+        bool Dial(string number);
+    }
+}
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phoneword
+{
+    interface IDialer
+    {
+    }
+}*/
